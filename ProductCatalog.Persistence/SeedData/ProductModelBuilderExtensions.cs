2b057fd15a8e5060e75fd7987101e21668b793d0
Mohamed Khalifa <m_e_khalifa@hotmail.com>
2020-12-19 18:04:14 +0200

    add default-image

--- ProductCatalog.Persistence/SeedData/ProductModelBuilderExtensions.cs
+++ ProductCatalog.Persistence/SeedData/ProductModelBuilderExtensions.cs
@@ -11,11 +11,11 @@ namespace ProductCatalog.Persistence.SeedData
         public static void SeedProducts(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>().HasData(
-                new Product { Id = 1, Name = "Product 1", Price = 10, Photo = "", LastUpdated = DateTime.Now },
-                new Product { Id = 2, Name = "Product 2", Price = 20, Photo = "", LastUpdated = DateTime.Now.AddDays(-1) },
-                new Product { Id = 3, Name = "Product 3", Price = 30, Photo = "", LastUpdated = DateTime.Now.AddDays(-2) },
-                new Product { Id = 4, Name = "Product 4", Price = 40, Photo = "", LastUpdated = DateTime.Now.AddDays(-2) },
-                new Product { Id = 5, Name = "Product 5", Price = 50, Photo = "", LastUpdated = DateTime.Now.AddDays(-3) }
+                new Product { Id = 1, Name = "Product 1", Price = 10, Photo = "Images/default-image.jpg", LastUpdated = DateTime.Now },
+                new Product { Id = 2, Name = "Product 2", Price = 20, Photo = "Images/default-image.jpg", LastUpdated = DateTime.Now.AddDays(-1) },
+                new Product { Id = 3, Name = "Product 3", Price = 30, Photo = "Images/default-image.jpg", LastUpdated = DateTime.Now.AddDays(-2) },
+                new Product { Id = 4, Name = "Product 4", Price = 40, Photo = "Images/default-image.jpg", LastUpdated = DateTime.Now.AddDays(-2) },
+                new Product { Id = 5, Name = "Product 5", Price = 50, Photo = "Images/default-image.jpg", LastUpdated = DateTime.Now.AddDays(-3) }
                 );
         }
     }
